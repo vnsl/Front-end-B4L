de783290d92415e8901531b291e0d83990fc922b
jeanrvos <velasco.jean@gmail.com>
2021-08-05 18:28:50 -0300

    inclui atualização do modal de editar perfil

--- b4l/src/componentes/ModalEditarUsuario/index.js
+++ b4l/src/componentes/ModalEditarUsuario/index.js
@@ -13,29 +13,22 @@ import InputSenha from '../InputSenha/index';
 import './index.css';
 
 import useAuth from '../../hook/useAuth';
-import { useHistory } from 'react-router-dom';
 
 export default function ModalEditarUsuario(props) {
   const classes = useStyles();
-  const history = useHistory();
   const [open, setOpen] = useState(false);
-  const { register, handleSubmit, reset } = useForm();
+  const { register, handleSubmit } = useForm();
   const [ erro, setErro ] = useState('');
   const [ carregando, setCarregando ] = useState(false);
-  const { token, categorias, setCategorias } = useAuth();
+  const { token, categorias, setCategorias, userPersistido } = useAuth();
   
   
   const recarregar = props.recarregar;
-  
-  const { id, nome, preco, descricao, ativo, permite_observacoes: permiteObserservacoes } = props.produtoInfo ?? '';
-  const [ produtoAtivo, setProdutoAtivo ] = useState(ativo);
-  const [ observacoes, setObservacoes ] = useState(false);
 
   const handleOpen = () => {
+    console.log(categorias);
     if (props.acao === 'Editar Perfil') {
       setErro('');
-      setProdutoAtivo(true);
-      setObservacoes(false);
     }
     setOpen(true);
     
@@ -66,59 +59,12 @@ export default function ModalEditarUsuario(props) {
       }, []);
 
   async function onSubmit(data) {
-    console.log(data);
     setCarregando(true);
     setErro('');
-        
+
         try {
-          if (props.acao === 'Novo produto') {
-            const resposta = await fetch('http://localhost:3000/produtos', {
-              method: 'POST',
-              body: JSON.stringify(data),
-              headers: {
-                'Content-type': 'application/json',
-                'Authorization': `Bearer ${token}`
-              }
-            })
-            
-            const dados = await resposta.json();
-            
-            setCarregando(false);
-            
-            if (!resposta.ok) {
-              setErro(dados);
-              return;
-            }
-          } else {
-            if (ativo !== produtoAtivo && !produtoAtivo) {
-              try {
-                const resposta = await fetch(`http://localhost:3000/produtos/${id}/desativar`, {
-                  method: 'POST',
-                  headers: {
-                    'Content-type': 'application/json',
-                    'Authorization': `Bearer ${token}`
-                  }
-                })
-                setCarregando(false);
-              } catch (error) {
-                setErro(error.message)
-              }
-            } else if (ativo !== produtoAtivo && produtoAtivo) {
-              try {
-                const resposta = await fetch(`http://localhost:3000/produtos/${id}/ativar`, {
-                  method: 'POST',
-                  headers: {
-                    'Content-type': 'application/json',
-                    'Authorization': `Bearer ${token}`
-                  }
-                })
-                setCarregando(false);
-              } catch (error) {
-                setErro(error.message)
-              }
-            }
-            if(data.nome || data.preco || data.descricao) {
-              const resposta = await fetch(`http://localhost:3000/produtos/${id}`, {
+          
+          const resposta = await fetch(`http://localhost:3000/usuarios/${userPersistido.id}`, {
               method: 'PUT',
               body: JSON.stringify(data),
               headers: {
@@ -126,30 +72,23 @@ export default function ModalEditarUsuario(props) {
                 'Authorization': `Bearer ${token}`
               }
             })
-            
-            const dados = await resposta.json();
-            
-            setCarregando(false);
-            history.push('/produtos2');
-            if (!resposta.ok) {
-              setErro(dados);
-              return;
-            }
-            }
-
-            if(data.nome && data.preco && data.descricao) {
-              setCarregando(false);
-              handleClose();
-            }
-            
+          
+          const dados = await resposta.json();
+          
+          setCarregando(false);
+          
+          if (!resposta.ok) {
+            setErro(dados);
+            return;
           }
           
           recarregar();
           handleClose();
-          
+
         } catch (error) {
           setErro(error.message)
         }
+
   }
 
   const body = (
@@ -158,44 +97,133 @@ export default function ModalEditarUsuario(props) {
         <div className={classes.content}>
           <div className={classes.fields}>
             <div className={classes.dadosUsuario}>
-              <TextField variant="outlined" key='nome' className='textarea' label="Nome de usuário" {...register('nome')} type='text' />
-              <TextField variant="outlined" key='email' className='textarea' label="Email" {...register('email')} type='text'/>
+              <TextField 
+                variant="outlined" 
+                key='nome' 
+                className='textarea' 
+                label="Nome de usuário" 
+                {...register('nome')} 
+                type='text' 
+                defaultValue={userPersistido.nome} 
+              />
+              <TextField 
+                variant="outlined" 
+                key='email' 
+                className='textarea' 
+                label="Email" 
+                {...register('email')} 
+                type='text' 
+                defaultValue={userPersistido.email} 
+              />
             </div>
             <div className="dados-restaurante" >
-              <TextField variant="outlined" key='restaurante.nome'className='textarea' label="Nome do restaurante" {...register('restaurante.nome')} type='text'/>
-              <TextField variant="outlined" key='restaurante.idCategoria' className='textarea' label="Categoria" {...register('restaurante.idCategoria')} select type='number'>
+              <TextField 
+                variant="outlined" 
+                key='restaurante.nome'
+                className='textarea' 
+                label="Nome do restaurante" 
+                {...register('restaurante.nome')} 
+                type='text' 
+                defaultValue={userPersistido.restaurante.nome} 
+            />
+              <TextField variant="outlined" 
+                key='restaurante.idCategoria' 
+                className='textarea' 
+                label="Categoria" 
+                {...register('restaurante.idCategoria')} 
+                select type='number'
+                defaultValue={userPersistido.restaurante.categoria_id}
+              >
                   {categorias.map((opcao) => (
                     <MenuItem key={opcao.id} value={opcao.id}>
                     {opcao.nome}
                     </MenuItem>
                     ))}
               </TextField>
-              <TextField variant="outlined" multiline rows={2} helperText="Máx: 50 caracteres" key='restaurante.descricao' className='textarea' label="Descrição" {...register('restaurante.descricao')} type='text'/>
-              <TextField variant="outlined" key='restaurante.taxaEntrega' className='textarea' label="Taxa de entrega" {...register('restaurante.taxaEntrega')} type='number' InputProps={{
-                startAdornment: <InputAdornment position="start">R$</InputAdornment>,
-              }} />
-              <TextField variant="outlined" key='restaurante.tempoEntregaEmMinutos' className='textarea' label="Tempo estimado de entrega" {...register('restaurante.tempoEntregaEmMinutos')} type='number'/>
-              <TextField variant="outlined" key='restaurante.valorMinimoPedido' className='textarea' label="Valor mínimo do pedido" {...register('restaurante.valorMinimoPedido')} type='number' InputProps={{
-                startAdornment: <InputAdornment position="start">R$</InputAdornment>,
-              }} /> 
+              <TextField 
+                variant="outlined" 
+                multiline rows={2} 
+                helperText="Máx: 50 caracteres" 
+                key='restaurante.descricao' 
+                className='textarea' 
+                label="Descrição" 
+                {...register('restaurante.descricao')} 
+                type='text'
+                defaultValue={userPersistido.restaurante.descricao}
+              />
+              <TextField 
+                variant="outlined" 
+                key='restaurante.taxaEntrega' 
+                className='textarea' 
+                label="Taxa de entrega" 
+                {...register('restaurante.taxaEntrega')} 
+                type='number' 
+                InputProps={{
+                  startAdornment: 
+                    <InputAdornment position="start">
+                      R$
+                    </InputAdornment>,
+                }} 
+                defaultValue={userPersistido.restaurante.taxa_entrega}
+              />
+              <TextField 
+                variant="outlined" 
+                key='restaurante.tempoEntregaEmMinutos' 
+                className='textarea' 
+                label="Tempo estimado de entrega" 
+                {...register('restaurante.tempoEntregaEmMinutos')} 
+                type='number'
+                defaultValue={userPersistido.restaurante.tempo_entrega_minutos}
+              />
+              <TextField 
+                variant="outlined" 
+                key='restaurante.valorMinimoPedido' 
+                className='textarea' 
+                label="Valor mínimo do pedido" 
+                {...register('restaurante.valorMinimoPedido')} 
+                type='number' 
+                InputProps={{
+                  startAdornment: 
+                    <InputAdornment position="start">
+                      R$
+                    </InputAdornment>,
+                }} 
+                defaultValue={userPersistido.restaurante.valor_minimo_pedido}
+              /> 
             </div>
-            <InputSenha register={() => register('senha')} label='Senha'/>
-            <InputSenha register={() => register('senhaRepetida')} label='Repita a senha'/>
-              {carregando && <Loading/>}
-              {erro && <Alert severity="error">{erro}</Alert>}
+            <InputSenha 
+              register={() => register('senha_atual')} 
+              label='Senha Atual'
+            />
+            <InputSenha 
+              register={() => register('senha')} 
+              label='Nova Senha'
+            />
+            <InputSenha 
+              register={() => register('senhaRepetida')} 
+              label='Repita a nova senha'
+            />
+            {carregando && <Loading/>}
+            {erro && <Alert severity="error">{erro}</Alert>}
           </div>
           <div className={classes.imgUpload}>
           </div>
         </div>
         <div className={classes.containerBotoes}>
             <div className={classes.botoes}>
-                <Button type="button" color="secondary" onClick={handleClose}>
-                    Cancelar
+                <Button 
+                    type="button" 
+                    color="secondary" 
+                    onClick={handleClose}>
+                      Cancelar
                 </Button>
-                <Button variant="contained" type="button" color="secondary" onClick={handleSubmit(onSubmit)}>
-                  Salvar alterações
-                </Button>
-                
+                <Button 
+                  variant="contained" 
+                  type="button" 
+                  color="secondary" 
+                  onClick={handleSubmit(onSubmit)}>
+                    Salvar alterações
+                </Button>                
             </div>
         </div>
     </div>
@@ -203,14 +231,13 @@ export default function ModalEditarUsuario(props) {
 
   return (
     <div>
-      {props.acao === 'Novo produto' ? 
-        <Button variant="contained" type="button" color="secondary" onClick={handleOpen}>
-        Adicionar produto ao cardápio
-        </Button> :
-        <Button variant="contained" type="button" color="secondary" onClick={handleOpen}>
-        Editar produto
-        </Button>
-      }
+      <Button 
+        variant="contained" 
+        type="button" 
+        color="secondary" 
+        onClick={handleOpen}>
+          Editar Perfil
+      </Button>
       <Modal
         open={open}
         onClose={handleClose}
